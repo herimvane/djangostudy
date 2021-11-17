406c863d055c7f5149018470a4a8df292836502d
herimvane <herimvane@gmail.com>
2021-11-17 13:46:33 +0800

    1

--- djangostudy/settings.py
+++ djangostudy/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'polls.apps.PollsConfig',
+    'modelstudy.apps.ModelstudyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
